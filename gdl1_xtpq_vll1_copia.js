6959154ea572cbd88647fd144939643498a4e8f5
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-14 17:29:55 -0600

    Add files via upload

---
+++ gdl1_xtpq_vll1_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "gdl1_xtpq_vll1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "F4M9+FH3, Tepic, Nayarit", "description": "X_TPQ1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -104.8810561, 21.4835833, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Manuel Corona 460A, Morelos y Pavón, 48290 Puerto Vallarta, Jal.", "description": "SPVLL1", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -105.2128674, 20.6674134, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "20.624283851536905, -103.2893056", "description": "GDL1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -103.2892967, 20.6243079, 0.0 ] } }
+]
+}
---
+++ seguras_gdl1_xtpq_vll1_copia.js
@@ -0,0 +1,10 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "seguras_gdl1_xtpq_vll1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex Grupo BAZEM", "description": "Carrtera Federal 200, Tepic - Puerto Vallarta Km 36, 69, 63704 Compostela, Nay.", "layer": "PEMEX_GDL_TPQ_VLL", "direccion": "Carrtera Federal 200, Tepic - Puerto Vallarta Km 36, 69, 63704 Compostela, Nay." }, "geometry": { "type": "Point", "coordinates": [ -104.9247398, 21.2328076, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "𝑷𝒆𝒎𝒆𝒙 𝑹𝒆𝒚 𝑵𝒂𝒚𝒂𝒓", "description": "Av Insurgentes 2171, Gustavo Díaz Ordaz, 63176 Tepic, Nay.", "layer": "PEMEX_GDL_TPQ_VLL", "direccion": "Av Insurgentes 2171, Gustavo Díaz Ordaz, 63176 Tepic, Nay." }, "geometry": { "type": "Point", "coordinates": [ -104.8764619, 21.4853916, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Petro Seven", "description": "Carr. Guadalajara-Nogales Km.13900, 45019 Zapopan, Jal.", "layer": "PEMEX_GDL_TPQ_VLL", "direccion": "Carr. Guadalajara-Nogales Km.13900, 45019 Zapopan, Jal." }, "geometry": { "type": "Point", "coordinates": [ -103.5027905, 20.7240528, 0.0 ] } }
+]
+}
